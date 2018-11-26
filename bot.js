8b61260b71561eaf48f3ef3e8f048d9ec1d6c02c
wented6 <44713416+wented6@users.noreply.github.com>
2018-11-26 16:33:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "-"// البرفكس هنا
+var prefix = ""// البرفكس هنا
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -35,7 +35,7 @@ client.on('ready', () => {
 const Discord = require('discord.js');
 const client = new Discord.Client();
 ////////////////////////////////
-const hngel1 = ["358247898997063710"]
+const hngel1 = ["452416139725635604"]
 const hngel = [`
                              
              **حياك الله ياعيني تنورنا
