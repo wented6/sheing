7ba381618747de55e07ebd550e8346e4960ec814
wented6 <44713416+wented6@users.noreply.github.com>
2018-11-26 16:43:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,43 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = ""// البرفكس هنا
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('By Wenteed')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
-
-  
-  
-
-
-});
- 
-
 ////////////////////////////////
 const hngel1 = ["jF6hyTK"]
 const hngel = [`
                              
-             **حياك الله ياعيني تنورنا
+**حياك الله ياعيني تنورنا
 سيرفر روابط سيرفرات ادخل وحط سيرفرك**
 https://discord.gg/qvXkBA8
 
@@ -75,6 +42,4 @@ ${hngel1}
 **`) 
 }).catch(console.error)
 })
-
- 
 client.login(process.env.BOT_TOKEN);
